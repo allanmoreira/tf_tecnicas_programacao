5bb9c8c0201cab026ba6bf650c6a1f71ad03d35a
Allan Moreira <allanfelipe.moreira@gmail.com>
2016-06-23 19:52:01 -0300

    Alteração diretório persistencia

--- sistema/CompanhiaAereaAcme/src/main/java/br/com/acme/gui/CtrlCompraPassagem.java
+++ sistema/CompanhiaAereaAcme/src/main/java/br/com/acme/gui/CtrlCompraPassagem.java
@@ -1,6 +1,6 @@
 package br.com.acme.gui;
 
-import br.com.acme.persistencia.derby.InicializadorBancoDadosDataSource;
+import br.com.acme.persistencia.postgre.InicializadorBancoDadosDataSource;
 
 public class CtrlCompraPassagem {
     public static void main(String[] args) throws Exception{
