6e6145900392ce24c8bd07e543464dc3ad359b44
Gabriell Araujo <ZK9RX6@users.noreply.github.com>
2016-06-23 21:10:17 -0300

    Update AssentoDAOPostgre.java

--- sistema/CompanhiaAereaAcme/src/main/java/br/com/acme/persistencia/postgre/AssentoDAOPostgre.java
+++ sistema/CompanhiaAereaAcme/src/main/java/br/com/acme/persistencia/postgre/AssentoDAOPostgre.java
@@ -18,7 +18,57 @@ public class AssentoDAOPostgre implements AssentoDAO {
 
     @Override
     public Assento selectById(int idAssento) throws AssentoDAOException {
-        return null;
+        Assento assento = null;
+        String sql = "SELECT \"passagens\".\"id_passagem\", \"passagens\".\"id_cliente_id\", "
+                + "\"passagens\".\"status\", \"passagens\".\"data_compra\", "
+                + "\"passagens\".\"preco_de_compra\", \"passagens\".\"promocoes_id\", "
+                + "\"passagens\".\"id_assento_id\", \"passagens\".\"id_voo_id\", "
+                + "\"clientes\".\"id_cliente\", \"clientes\".\"nome\", \"clientes\".\"rg\", "
+                + "\"clientes\".\"cpf\", \"clientes\".\"numero_passaporte\", \"assentos\".\"id_assento\", "
+                + "\"assentos\".\"id_aeronave_id\", \"assentos\".\"tipo\", \"aeronaves\".\"id_aeronave\", "
+                + "\"aeronaves\".\"fabricante\", \"aeronaves\".\"modelo\", \"aeronaves\".\"numero_de_assentos\", "
+                + "\"voos\".\"id_voo\", \"voos\".\"id_linha_aerea_id\", \"voos\".\"id_aeronaves_id\", "
+                + "\"voos\".\"tempo_estimado\", \"voos\".\"id_partida_id\", \"voos\".\"id_chegada_id\", "
+                + "\"linhas_aereas\".\"id_linha_aerea\", \"linhas_aereas\".\"nome\", \"linhas_aereas\".\"id_alianca_id\", "
+                + "T7.\"id_aeronave\", T7.\"fabricante\", T7.\"modelo\", T7.\"numero_de_assentos\", \"partidas\".\"id_partida\", "
+                + "\"partidas\".\"data_partida\", \"partidas\".\"hora_partida\", \"partidas\".\"id_aeroporto_id\", "
+                + "\"aeroportos\".\"id_aeroporto\", \"aeroportos\".\"latitude\", \"aeroportos\".\"longitude\", "
+                + "\"aeroportos\".\"nome\", \"chegadas\".\"id_chegada\", \"chegadas\".\"data_chegada\", \"chegadas\".\"hora_chegada\", "
+                + "\"chegadas\".\"id_aeroporto_id\", T11.\"id_aeroporto\", T11.\"latitude\", T11.\"longitude\", "
+                + "T11.\"nome\" FROM \"passagens\" INNER JOIN \"clientes\" ON "
+                + "(\"passagens\".\"id_cliente_id\" = \"clientes\".\"id_cliente\") INNER JOIN "
+                + "\"assentos\" ON (\"passagens\".\"id_assento_id\" = \"assentos\".\"id_assento\") "
+                + "INNER JOIN \"aeronaves\" ON (\"assentos\".\"id_aeronave_id\" = \"aeronaves\".\"id_aeronave\") "
+                + "INNER JOIN \"voos\" ON (\"passagens\".\"id_voo_id\" = \"voos\".\"id_voo\") "
+                + "INNER JOIN \"linhas_aereas\" ON (\"voos\".\"id_linha_aerea_id\" = \"linhas_aereas\".\"id_linha_aerea\") "
+                + "INNER JOIN \"aeronaves\" T7 ON (\"voos\".\"id_aeronaves_id\" = T7.\"id_aeronave\") "
+                + "INNER JOIN \"partidas\" ON (\"voos\".\"id_partida_id\" = \"partidas\".\"id_partida\") "
+                + "INNER JOIN \"aeroportos\" ON (\"partidas\".\"id_aeroporto_id\" = \"aeroportos\".\"id_aeroporto\") "
+                + "INNER JOIN \"chegadas\" ON (\"voos\".\"id_chegada_id\" = \"chegadas\".\"id_chegada\") "
+                + "INNER JOIN \"aeroportos\" T11 ON (\"chegadas\".\"id_aeroporto_id\" = T11.\"id_aeroporto\")"
+				+ " WHERE \"assentos\".\"id_assento\" = ?";
+        try (Connection conexao = InicializadorBancoDadosDataSource.conectarBd()) {
+            try (PreparedStatement comando = conexao.prepareStatement(sql)) {
+				comando.setString(1, idAssento);
+                try (ResultSet resultado = comando.executeQuery()) {
+                    while (resultado.next()) {
+						int id_assento = resultado.getInt(14);
+						String tipo = resultado.getString(15);
+						int aeronave_id = resultado.getInt(16);
+						
+                        assento = new Assento(id_assento,tipo,aeronave_id);                        
+                    }
+                }
+            }
+        } catch (AssentoDAOException ex) {
+            throw new AssentoDAOException("Falha na busca. " + ex.getMessage());
+        } catch (SQLException ex) {
+            Logger.getLogger(AssentoDAOPostgre.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(AssentoDAOPostgre.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return assento;
+    }
     }
 
     @Override
@@ -55,7 +105,11 @@ public class AssentoDAOPostgre implements AssentoDAO {
             try (PreparedStatement comando = conexao.prepareStatement(sql)) {
                 try (ResultSet resultado = comando.executeQuery()) {
                     while (resultado.next()) {
-                        
+						int id_assento = resultado.getInt(14);
+						String tipo = resultado.getString(15);
+						int aeronave_id = resultado.getInt(16);
+						
+                        todosAssentos.add(new Assento(id_assento,tipo,aeronave_id);                        
                     }
                 }
             }
@@ -71,6 +125,55 @@ public class AssentoDAOPostgre implements AssentoDAO {
 
     @Override
     public List<Assento> selectAll(int idVoo) throws AssentoDAOException {
-        return null;
+        List<Assento> todosAssentos = new ArrayList<>();
+        String sql = "SELECT \"passagens\".\"id_passagem\", \"passagens\".\"id_cliente_id\", "
+                + "\"passagens\".\"status\", \"passagens\".\"data_compra\", "
+                + "\"passagens\".\"preco_de_compra\", \"passagens\".\"promocoes_id\", "
+                + "\"passagens\".\"id_assento_id\", \"passagens\".\"id_voo_id\", "
+                + "\"clientes\".\"id_cliente\", \"clientes\".\"nome\", \"clientes\".\"rg\", "
+                + "\"clientes\".\"cpf\", \"clientes\".\"numero_passaporte\", \"assentos\".\"id_assento\", "
+                + "\"assentos\".\"id_aeronave_id\", \"assentos\".\"tipo\", \"aeronaves\".\"id_aeronave\", "
+                + "\"aeronaves\".\"fabricante\", \"aeronaves\".\"modelo\", \"aeronaves\".\"numero_de_assentos\", "
+                + "\"voos\".\"id_voo\", \"voos\".\"id_linha_aerea_id\", \"voos\".\"id_aeronaves_id\", "
+                + "\"voos\".\"tempo_estimado\", \"voos\".\"id_partida_id\", \"voos\".\"id_chegada_id\", "
+                + "\"linhas_aereas\".\"id_linha_aerea\", \"linhas_aereas\".\"nome\", \"linhas_aereas\".\"id_alianca_id\", "
+                + "T7.\"id_aeronave\", T7.\"fabricante\", T7.\"modelo\", T7.\"numero_de_assentos\", \"partidas\".\"id_partida\", "
+                + "\"partidas\".\"data_partida\", \"partidas\".\"hora_partida\", \"partidas\".\"id_aeroporto_id\", "
+                + "\"aeroportos\".\"id_aeroporto\", \"aeroportos\".\"latitude\", \"aeroportos\".\"longitude\", "
+                + "\"aeroportos\".\"nome\", \"chegadas\".\"id_chegada\", \"chegadas\".\"data_chegada\", \"chegadas\".\"hora_chegada\", "
+                + "\"chegadas\".\"id_aeroporto_id\", T11.\"id_aeroporto\", T11.\"latitude\", T11.\"longitude\", "
+                + "T11.\"nome\" FROM \"passagens\" INNER JOIN \"clientes\" ON "
+                + "(\"passagens\".\"id_cliente_id\" = \"clientes\".\"id_cliente\") INNER JOIN "
+                + "\"assentos\" ON (\"passagens\".\"id_assento_id\" = \"assentos\".\"id_assento\") "
+                + "INNER JOIN \"aeronaves\" ON (\"assentos\".\"id_aeronave_id\" = \"aeronaves\".\"id_aeronave\") "
+                + "INNER JOIN \"voos\" ON (\"passagens\".\"id_voo_id\" = \"voos\".\"id_voo\") "
+                + "INNER JOIN \"linhas_aereas\" ON (\"voos\".\"id_linha_aerea_id\" = \"linhas_aereas\".\"id_linha_aerea\") "
+                + "INNER JOIN \"aeronaves\" T7 ON (\"voos\".\"id_aeronaves_id\" = T7.\"id_aeronave\") "
+                + "INNER JOIN \"partidas\" ON (\"voos\".\"id_partida_id\" = \"partidas\".\"id_partida\") "
+                + "INNER JOIN \"aeroportos\" ON (\"partidas\".\"id_aeroporto_id\" = \"aeroportos\".\"id_aeroporto\") "
+                + "INNER JOIN \"chegadas\" ON (\"voos\".\"id_chegada_id\" = \"chegadas\".\"id_chegada\") "
+                + "INNER JOIN \"aeroportos\" T11 ON (\"chegadas\".\"id_aeroporto_id\" = T11.\"id_aeroporto\")"
+				+ " WHERE \"voos\".\"id_voo\" = ?";
+        try (Connection conexao = InicializadorBancoDadosDataSource.conectarBd()) {
+            try (PreparedStatement comando = conexao.prepareStatement(sql)) {
+				comando.setString(1, idVoo);
+                try (ResultSet resultado = comando.executeQuery()) {
+                    while (resultado.next()) {
+						int id_assento = resultado.getInt(14);
+						String tipo = resultado.getString(15);
+						int aeronave_id = resultado.getInt(16);
+						
+                        todosAssentos.add(new Assento(id_assento,tipo,aeronave_id);                        
+                    }
+                }
+            }
+        } catch (AssentoDAOException ex) {
+            throw new AssentoDAOException("Falha na busca. " + ex.getMessage());
+        } catch (SQLException ex) {
+            Logger.getLogger(AssentoDAOPostgre.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(AssentoDAOPostgre.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return todosAssentos;
     }
 }
